d48ee7c70956318694dbae69430686b4af5fc96e
Oleg Kosin <olegkosin@rambler.ru>
2018-12-27 14:02:39 +0300

    text box undefined?

--- RSATU1/static/chat_script.js
+++ RSATU1/static/chat_script.js
@@ -1,10 +1,16 @@
 function sendMessage() {
     console.log("qwe");
-    var mess_params = new FormData();
-    mess_params.append("text", this.message_text)
-    mess_params.append("chat", appData.chat_id)
-    mess_params.append("author", appData.author)
-    mess_params.append("dateTime", '0')
+
+    let mess_params = new FormData();
+    console.log(this.message_text.textContent);
+    mess_params.append("text", this.message_text.textContent);
+    mess_params.append("chat", appData.chat_id.val());
+    mess_params.append("author", appData.author.val());
+    mess_params.append("dateTime", '0');
+
+    let url = 'chats/' + appData.chat_id;
+
+    $.post(url, {text: this.message_text}, onAjaxSuccess);
 
     $.get({
         url: 'chats/' + appData.chat_id,
@@ -15,3 +21,8 @@ function sendMessage() {
     console.log("qwe");
 }
 
+
+function onAjaxSuccess(data) {
+    // Здесь мы получаем данные, отправленные сервером и выводим их на экран.
+    alert(data);
+}
